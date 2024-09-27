1562ac46af099be4801bdd01dcde70711f811f81
Muhammad Aiman Furqon <aimanconnects@gmail.com>
2024-09-27 22:42:32 +0700

    feat: added student model

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [Display(Name="Student Name")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Display(Name="Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name="Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
